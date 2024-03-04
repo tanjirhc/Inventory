80dfcfadcfbb57e27be335867077181aec1b2801
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-04 13:24:36 +0600

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Login2()
+        {
+            return View();
+        }
+    }
+}
