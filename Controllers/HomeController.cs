1393ffad0dfd18b33f6e51f8138002310b7f3d75
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-04 15:58:03 +0600

    table, tr Created in Dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inventory.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,8 @@ namespace Inventory.Controllers
 
         public ActionResult Dashboard()
         {
+            List<BaseEquipment> plstData = BaseEquipment.ListEquipmentData();
+            ViewBag.plstData = plstData;
             return View();
         }
 
---
+++ Models/BaseEquipment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Models
+{
+    public class BaseEquipment
+    {
+        public string Name { get; set; }
+        public int EcCount { get; set; }
+
+        public DateTime EntryDate { get; set;}
+
+        public List<BaseEquipment> ListEquipment { get; set; }
+
+        public BaseEquipment() 
+        {
+            ListEquipment = new List<BaseEquipment>(); 
+        }
+
+        public static List<BaseEquipment> ListEquipmentData()
+        {
+            List<BaseEquipment> plstData = new List<BaseEquipment>();
+
+            BaseEquipment baseEquipment = new BaseEquipment();
+            baseEquipment.Name = "Laptop";
+            baseEquipment.EcCount = 5;
+            baseEquipment.EntryDate = DateTime.Now.Date;
+            plstData.Add(baseEquipment);
+
+            baseEquipment = new BaseEquipment();
+            baseEquipment.Name = "Mouse";
+            baseEquipment.EcCount = 7;
+            baseEquipment.EntryDate = DateTime.Now.Date;
+            plstData.Add(baseEquipment);
+
+            baseEquipment = new BaseEquipment();
+            baseEquipment.Name = "Keyboard";
+            baseEquipment.EcCount = 10;
+            baseEquipment.EntryDate = DateTime.Now.Date;
+            plstData.Add(baseEquipment);
+
+
+            return plstData;
+        }
+    }
+}
