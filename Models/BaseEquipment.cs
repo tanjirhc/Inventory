fd133903c96326430d66f033ef7b32301378c912
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-04 16:52:11 +0600

    Data binding in loop by Razor

--- Models/BaseEquipment.cs
+++ Models/BaseEquipment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc.Routing.Constraints;
 
 namespace Inventory.Models
 {
@@ -23,25 +24,15 @@ namespace Inventory.Models
         {
             List<BaseEquipment> plstData = new List<BaseEquipment>();
 
-            BaseEquipment baseEquipment = new BaseEquipment();
-            baseEquipment.Name = "Laptop";
-            baseEquipment.EcCount = 5;
-            baseEquipment.EntryDate = DateTime.Now.Date;
-            plstData.Add(baseEquipment);
-
-            baseEquipment = new BaseEquipment();
-            baseEquipment.Name = "Mouse";
-            baseEquipment.EcCount = 7;
-            baseEquipment.EntryDate = DateTime.Now.Date;
-            plstData.Add(baseEquipment);
-
-            baseEquipment = new BaseEquipment();
-            baseEquipment.Name = "Keyboard";
-            baseEquipment.EcCount = 10;
-            baseEquipment.EntryDate = DateTime.Now.Date;
-            plstData.Add(baseEquipment);
-
-
+            for(int i=0;i<50;i++)
+            {
+                BaseEquipment baseEquipment = new BaseEquipment();
+                baseEquipment.Name = "Laptop-"+i.ToString();
+                baseEquipment.EcCount = 5+i;
+                baseEquipment.EntryDate = DateTime.Now.Date;
+                plstData.Add(baseEquipment);
+            }
+            
             return plstData;
         }
     }
