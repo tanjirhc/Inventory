fdfb49867e12012c607acf40d4a28360ec5208ca
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-15 00:13:58 +0600

    Login Page success/ Failed message showing

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -16,18 +16,25 @@ namespace Inventory.Controllers
         [HttpPost]
         public ActionResult Login(string btnSubmit, string txtUserName, string txtPassword)
         {
+            string LoginMsg = "";
             if (btnSubmit == "Login")
             {
                 if (txtUserName == "Tanjir" && txtPassword == "1234")
                 {
                     Session["User"] = "Tanjir";
+                    LoginMsg = "Login Success";
                     return RedirectToAction("Dashboard", "Home");
-                }                
+                }
+                else
+                {
+                    LoginMsg = "Failed, Username/Password Not Matched";
+                }
             }
             else if (btnSubmit == "Forget Password")
             {
                 return RedirectToAction("forget", "Account");
             }
+            ViewBag.LoginMsg = LoginMsg;
             return View();
         }
         [HttpPost]
