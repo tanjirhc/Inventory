19d6c780e2f3ca6f82ed288c7f342a13d30789db
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-16 21:25:42 +0600

    CommandTimeOut unlimited

--- Models/BaseAccount.cs
+++ Models/BaseAccount.cs
@@ -22,7 +22,7 @@ namespace Inventory.Models
             SqlCommand cmd = sqlConnection.CreateCommand();
             cmd.CommandText = "";
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-
+            cmd.CommandTimeout = 0;
             if (this.UserName == "Tanjir" && this.Password == "1234")
             {
                 return true;
